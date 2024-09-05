be4c01782c5b1b16427d17e0a8d3e113959f93ac
MathiasTensor <tensor1995@gmail.com>
2024-09-05 14:22:13 +0200

    Update unfolding.jl
    This is needed for compute_spectrum function is spectralutils.jl

--- src/spectra/unfolding.jl
+++ src/spectra/unfolding.jl
@@ -1,6 +1,22 @@
 
-
 corner_correction(corner_angles) =  sum([(pi^2 - c^2)/(24*pi*c) for c in corner_angles])
 
-weyl_law(A,L,k) = (A .* k.^2 .- L .* k)./(4*pi)
-weyl_law(A,L,k,corner_angles) = weyl_law(A,L,k) .- corner_correction(corner_angles)
+weyl_law(k,A,L) =  @. (A * k^2 - L * k)/(4*pi)
+weyl_law(k,A,L,corner_angles) =  weyl_law(k,A,L) .+ corner_correction(corner_angles)
+
+
+function k_at_state(state, A, L)
+    a = A
+    b = -L
+    c = -state*4*pi
+    dis = sqrt(b^2-4*a*c)
+    return (-b+dis)/(2*a)
+end
+
+function k_at_state(state, A, L, corner_angles)
+    a = A
+    b = -L
+    c = (corner_correction(corner_angles)-state)*4*pi 
+    dis = sqrt(b^2-4*a*c)
+    return (-b+dis)/(2*a)
+end
